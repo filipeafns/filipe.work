16585d56b109d26b1ca91d5fd45053f7947e98c5
flipsoares <filipe.afns@gmail.com>
2022-04-04 21:00:50 -0300

    Updates config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
