c72abac5d9b4208a8aca07ad28669d480ad17fd6
flipsoares <filipe.afns@gmail.com>
2022-05-01 18:27:23 -0300

    Styles Nav [WIP]

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,23 +1,23 @@
 import Link from 'next/link'
 
+import style from './nav.module.scss';
+
 export default function Nav() {
     return (
-        <header>
-            <nav>
+        <>
+            <nav className={style.nav}>
                 <ul>
                 <li><Link href="/"><a>FS</a></Link></li>
-                <li><Link href="/about"><a>About Me</a></Link></li>
-                <li><Link href="/works"><a>Works</a></Link></li>
-                <li><Link href="/contact"><a>Contact</a></Link></li>
+                <li><Link href="/about"><a>↳ About Me</a></Link></li>
+                <li><Link href="/works"><a>↳ Works</a></Link></li>
+                <li><Link href="/contact"><a>↳ Contact</a></Link></li>
                 </ul>
-                </nav>
                 <span>Filipe Soares</span>
-                <nav>
                 <ul>
-                <li>Linkedin -></li>
-                <li>Instagram -></li>
-                </ul>
+                <li><a href="http://linkedin.com/in/fisoares" target="_blank">Linkedin -></a></li>
+                <li><a href="http://instagram.com/filipeafns/" target="_blank">Instagram -></a></li>
+                </ul> 
             </nav>
-        </header>
+        </>
     )
 }
