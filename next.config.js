55c379efd292d0325cfaaf958b48b9e21dec6cc4
flipsoares <filipe.afns@gmail.com>
2022-05-07 15:04:54 -0300

    Ignores ESLint

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
 
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
