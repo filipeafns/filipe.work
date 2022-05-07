131dd4ef4a3025acfa3bd412ca24b3e70500655b
flipsoares <filipe.afns@gmail.com>
2022-05-07 15:07:19 -0300

    Updates again Next Config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ module.exports = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+          fs: false
+      }
+  }
     return config;
   },
   eslint: {
@@ -12,6 +17,7 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
