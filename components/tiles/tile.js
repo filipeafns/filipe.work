558301e2c50ebdaebb0ec5a280018fc2e90c82b7
flipsoares <filipe.afns@gmail.com>
2022-05-07 16:47:09 -0300

    Adds Next Image

--- components/tiles/tile.js
+++ components/tiles/tile.js
@@ -1,14 +1,21 @@
 import style from './tile.module.scss';
 import cn from 'classnames';
 
+import Image from 'next/image';
+
 export default function Tile({company, year, description}) {
     return (
-      <li className={cn(style.tile)} >
-        <a href="">
-          <p>{company}</p>
-          <legend>{year}</legend>
-          <span>{description}</span>
-        </a>
-      </li>
+      <>
+        <li className={cn(style.tile)} >
+            <p>{company}</p>
+            <legend>{year}</legend>
+            <span>{description}</span>
+            <Image
+              src="/images/works/dyramid.png"
+              alt="Case Image"
+              layout="fill"
+          />
+        </li>
+      </>
     )
   }
