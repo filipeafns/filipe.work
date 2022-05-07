eeb01f3a8c1309f25783f574ac9547fd3486a870
flipsoares <filipe.afns@gmail.com>
2022-05-06 23:38:05 -0300

    Removes 3d

--- components/header/header.js
+++ components/header/header.js
@@ -1,17 +1,18 @@
 import cn from 'classnames';
 import style from './header.module.scss';
-import dynamic from 'next/dynamic';
 
-const Spline = dynamic(() => import('@splinetool/react-spline'), {
-  ssr: false,
-});
+// import dynamic from 'next/dynamic';
+
+// const Spline = dynamic(() => import('@splinetool/react-spline'), {
+//   ssr: false,
+// });
 
 export default function Header() {
     return (
         <header  className={style.header}>
           <section className={cn(style.title, 'grid container')}>
             <h1>Hey, I'm Filipe Soares. I'm a Product and Visual Designer.</h1>
-            <Spline scene="https://prod.spline.design/rlUVl0RxykOsS2kZ/scene.splinecode" />
+            {/* <Spline scene="https://prod.spline.design/rlUVl0RxykOsS2kZ/scene.splinecode" /> */}
           </section>
         </header>
     )
