42f41d5db4f7927ae5d87fa29b135423e31fe803
flipsoares <filipe.afns@gmail.com>
2022-05-10 21:42:29 -0300

    Updates Exploring Page

--- components/section/exploring.js
+++ components/section/exploring.js
@@ -1,5 +1,12 @@
 import style from './exploring.module.scss';
-import Tile from '../tiles/tile';
+import cn from 'classnames';
+
+import Image from 'next/image';
+
+import lab1 from '../../public/images/lab/lab1.png';
+import lab2 from '../../public/images/lab/lab2.png';
+import lab3 from '../../public/images/lab/lab3.png';
+import lab4 from '../../public/images/lab/lab4.png';
 
 export default function Exploring() {
     return (
@@ -8,18 +15,54 @@ export default function Exploring() {
           <h3>Design for me has always been about exploring possibilities, either in Print, Code or Beyond. I Want to bring amazing things to the world.</h3>
           <h4>Recent Exploration</h4>
           <ul>
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
-            <Tile />
+            <li className={cn(style.tile, style.square)}>
+                <div>
+                  <p></p>
+                  <legend></legend>
+                  <span></span>
+                </div>
+                <Image
+                  src={lab1}
+                  layout="fill"
+                  placeholder='blur'
+                />
+            </li>
+            <li className={cn(style.tile, style.square)}>
+                <div>
+                  <p></p>
+                  <legend></legend>
+                  <span></span>
+                </div>
+                <Image
+                  src={lab2}
+                  layout="fill"
+                  placeholder='blur'
+                />
+            </li>
+            <li className={cn(style.tile, style.square)}>
+                <div>
+                  <p></p>
+                  <legend></legend>
+                  <span></span>
+                </div>
+                <Image
+                  src={lab3}
+                  layout="fill"
+                  placeholder='blur'
+                />
+            </li>
+            <li className={cn(style.tile, style.square)}>
+                <div>
+                  <p></p>
+                  <legend></legend>
+                  <span></span>
+                </div>
+                <Image
+                  src={lab4}
+                  layout="fill"
+                  placeholder='blur'
+                />
+            </li>
           </ul>
         </section>
     )
