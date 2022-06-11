813a3b623216420b85592b1e1d3b8a1ec2b285a0
flipsoares <filipe.afns@gmail.com>
2022-06-11 19:31:02 -0300

    Updates navigation

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -35,17 +35,17 @@ export default function Nav({ onClick }) {
                 <div>
                     <div className={style.logo} role="presentation">
                         <svg role="presentation" width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M37.5 75C58.2107 75 75 58.2107 75 37.5C75 16.7893 58.2107 0 37.5 0C16.7893 0 0 16.7893 0 37.5C0 58.2107 16.7893 75 37.5 75ZM57.7148 17.8711H20.2148V32.2266V46.2891V60.6445H30.1758V46.2891H41.0156V32.2266H57.7148V17.8711Z" fill="black" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M37.5 75C58.2107 75 75 58.2107 75 37.5C75 16.7893 58.2107 0 37.5 0C16.7893 0 0 16.7893 0 37.5C0 58.2107 16.7893 75 37.5 75ZM57.7148 17.8711H20.2148V32.2266V46.2891V60.6445H30.1758V46.2891H41.0156V32.2266H57.7148V17.8711Z" />
                         </svg>
                         <h4>Filipe Soares</h4>
                     </div>
                     <div role="navigation" aria-label="Main Navigation" className={cn({ [style.navmodal]: true, [style.open]: isOpen })}>
-                        {/* <ul role="presentation">
+                        <ul role="presentation">
                             <li role="presentation"><Link href="/" onClick={handleCloseClick}><a>Home</a></Link></li>
                             <li role="presentation"><Link href="/about" onClick={handleCloseClick}><a>↳ About Me</a></Link></li>
-                            <li role="presentation"><Link href="/lab" onClick={handleCloseClick}><a>↳ Lab</a></Link></li>
-                            <li role="presentation"><Link href="/contact"onClick={handleCloseClick}><a>↳ Contact</a></Link></li>
-                        </ul> */}
+                            {/* <li role="presentation"><Link href="/lab" onClick={handleCloseClick}><a>↳ Lab</a></Link></li>
+                            <li role="presentation"><Link href="/contact"onClick={handleCloseClick}><a>↳ Contact</a></Link></li> */}
+                        </ul>
                         <ul role="presentation" className={style.social}>
                             <li role="presentation"><a href="http://linkedin.com/in/fisoares" target="_blank">Linkedin -></a></li>
                             <li role="presentation"><a href="http://instagram.com/filipeafns/" target="_blank">Instagram -></a></li>
@@ -60,29 +60,3 @@ export default function Nav({ onClick }) {
         </>
     )
 }
-
-
-// export default function Nav() {
-//     return (
-//         <>
-//             <nav className={style.nav}>
-//                 <div>
-//                     <p>Filipe Soares</p>
-//                     <div className={cn(style.navmodal, style.isclosed)}>
-//                         <ul>
-//                             <li><Link href="/"><a>Home</a></Link></li>
-//                             <li><Link href="/about"><a>↳ About Me</a></Link></li>
-//                             <li><Link href="/lab"><a>↳ Lab</a></Link></li>
-//                             <li><Link href="/contact"><a>↳ Contact</a></Link></li>
-//                         </ul>
-//                         <ul className={style.social}>
-//                             <li><a href="http://linkedin.com/in/fisoares" target="_blank">Linkedin -></a></li>
-//                             <li><a href="http://instagram.com/filipeafns/" target="_blank">Instagram -></a></li>
-//                         </ul> 
-//                     </div>
-//                 </div>
-//                 <button>Open/close</button>
-//             </nav>
-//         </>
-//     )
-// }
