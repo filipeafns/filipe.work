c303637ae3119f242a0978bc061bb32a170b0af4
flipsoares <filipe.afns@gmail.com>
2022-07-01 22:26:00 -0300

    Removes theme meta

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
 
         <title>Filipe Soares — Product, Branding and Visual Designer.</title>
 
-        <meta name="theme-color" content="#211d1d" />
         <meta name="title" content="Filipe Soares — Product, Branding and Visual Designer." />
         <meta name="description" content="Hey, I'm Filipe Soares, Senior Visual and Interaction designer, extremely curious, creative, and passionate about solving problems. For the past eleven years, I've had the chance to explore the most exciting fields. I started as a Graphic Designer, gained experience as Art Director, and later become an Interaction designer. I'm triggered by the opportunity to forge great experiences and engage people with the skills I've acquired over the years in Branding, Motion Design, Illustration and, Interaction." />
 
