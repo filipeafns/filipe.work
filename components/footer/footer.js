f66f75619f9ea1fc3498a82a6de00bebc8219341
flipsoares <filipe.afns@gmail.com>
2022-07-17 18:54:56 -0300

    Adds fixed footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -4,14 +4,16 @@ import style from './footer.module.scss';
 export default function Footer() {
     return (
         <footer className={style.footer}>
-            <h2>Bye</h2>
             <div>
+                <h2>Bye</h2>
                 <div>
-                    <h4>Will I ever hear from you Again?</h4>
-                    <p>Get in touch filipe.afns@gmail.com</p>
-                </div>
-                <div>
-                    <p>Designed In Figma, Coded by Me (HTML & CSS), Built with NEXT.JS and hosted at VERCEL.</p>
+                    <div>
+                        <h4>Will I ever hear from you Again?</h4>
+                        <p>Get in touch filipe.afns@gmail.com</p>
+                    </div>
+                    <div>
+                        <p>Designed In Figma, Coded by Me (HTML & CSS), Built with NEXT.JS and hosted at VERCEL.</p>
+                    </div>
                 </div>
             </div>
         </footer>
